589dfeb779e51eb92f0e7a4b34e0693d45da9a87
Horst Zaepfle <horst.zaepfle@gmail.com>
2017-09-10 00:52:33 +0200

    using a library

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+
+		"github.com/zaepfle/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
